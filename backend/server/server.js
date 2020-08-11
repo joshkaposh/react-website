6e1161b18e20a5151e745259556e0090be22da78
Josh Dulisse <rd.josh09@gmail.com>
2020-08-11 13:25:11 -0300

    august-11

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,42 +1,45 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 4000;
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
-const mongoose = require('mongoose');
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 
+// change mongoose.connect = ef_conference
 
+//
+// "mongodb://efadmin:efadmin2468@35.182.202.94:27017/efconference"
 
-mongoose.connect('mongodb://localhost:27017/main',
-    { useNewUrlParser: true, useUnifiedTopology: true}
-)
+mongoose.connect("mongodb://localhost:27017/main", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function() {
-    console.log('Successfully connected to MongoDB!')
-}) 
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+	console.log("Successfully connected to MongoDB!");
+});
 
 const app = express();
 app.use(cors());
 
 // Mongoose Schema
 const roomSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  group: String,
-  owner: String
-})
+	title: String,
+	description: String,
+	group: String,
+	owner: String,
+});
 const userSchema = new mongoose.Schema({
-  name: String,
-  password: String,
-  group: String,
-  role: String,
-})
+	name: String,
+	password: String,
+	group: String,
+	role: String,
+});
 
-const userModel = mongoose.model('User', userSchema)
-const roomModel = mongoose.model('Room', roomSchema)
-
+const userModel = mongoose.model("User", userSchema);
+const roomModel = mongoose.model("Room", roomSchema);
 
 const typeDefs = buildSchema(`
   type Query {
@@ -53,9 +56,7 @@ const typeDefs = buildSchema(`
     deleteUser(name: String! password: String!): User!
     deleteRoom(id: ID!) : Room!
     updateRoom(id: ID! title: String description: String group: String owner: String) : Room!
-
   } 
-
 
   type Room {
     id: ID!
@@ -78,81 +79,111 @@ const typeDefs = buildSchema(`
 
 // GraphQL Root Resolver functions
 const root = {
-    rooms: async () => {
-      const documents = await roomModel.find()
-      console.log(documents)
-      return documents;
-    },
-    room: async ({id}) => {
-      const document = await roomModel.findById(id)
-      console.log(document)
-      return document;
-    },
-    users:  async () => {
-      const documents = await userModel.find()
-      console.log(documents)
-      return documents;
-    },
-    user: async ({name, password}) => {
-        const document = await userModel.findOne({name: name, password: password})
-        console.log(document);
-        return document;
-      },
-    userRooms: async ({name}) => {
-      const documents = await roomModel.find({owner: name})
-      console.log(documents);
-      return documents;
-    },
-    addUser: async ({name, password, group, role}) => {
-      const document = new userModel({name: name, password: password, group: group, role: role})
-      await document.save((err, user) => {
-        if(err) return console.error(err);
-        console.log(`User ${user.name} added to database`);
-      })
-      return document;
-    },
-    addRoom: async({title, description, group, owner}) => {
-      const document = new roomModel({ title: title, description: description, group: group, owner: owner});
-      await document.save((err, room) => {
-        if(err) return console.error(err);
-        console.log(`Room ${room.title} group: ${room.group} added to database`);
-      })
-      return document;
-    },
-    deleteUser: async ({name, password}) => {
-        const document = await userModel.findOneAndDelete({name: name, password: password})
-        console.log(`User ${name} deleted from database`);
-        return document;
-    },
+	rooms: async () => {
+		const documents = await roomModel.find();
+		console.log(documents);
+		return documents;
+	},
+	room: async ({ id }) => {
+		const document = await roomModel.findById(id);
+		console.log(document);
+		return document;
+	},
+	users: async () => {
+		const documents = await userModel.find();
+		console.log(documents);
+		return documents;
+	},
+	user: async ({ name, password }) => {
+		const document = await userModel.findOne({
+			name: name,
+			password: password,
+		});
+		console.log(document);
+		return document;
+	},
+	userRooms: async ({ name }) => {
+		const documents = await roomModel.find({ owner: name });
+		console.log(documents);
+		return documents;
+	},
+	addUser: async ({ name, password, group, role }) => {
+		const document = new userModel({
+			name: name,
+			password: password,
+			group: group,
+			role: role,
+		});
+		await document.save((err, user) => {
+			if (err) return console.error(err);
+			console.log(`User ${user.name} added to database`);
+		});
+		return document;
+	},
+	addRoom: async ({ title, description, group, owner }) => {
+		const document = new roomModel({
+			title: title,
+			description: description,
+			group: group,
+			owner: owner,
+		});
+		await document.save((err, room) => {
+			if (err) return console.error(err);
+			console.log(
+				`Room ${room.title} group: ${room.group} added to database`
+			);
+		});
+		return document;
+	},
+	deleteUser: async ({ name, password }) => {
+		const document = await userModel.findOneAndDelete({
+			name: name,
+			password: password,
+		});
+		console.log(`User ${name} deleted from database`);
+		return document;
+	},
 
-    deleteRoom: async ({id}) => {
-      const document = await roomModel.findByIdAndDelete(id)
-      console.log(document);
-      console.log(`room ${id} was deleted from database`);
-      return document;
-  },
+	deleteRoom: async ({ id }) => {
+		const document = await roomModel.findByIdAndDelete(id);
+		console.log(document);
+		console.log(`room ${id} was deleted from database`);
+		return document;
+	},
 
-  updateRoom: async ({id, title, description, group, owner}) => {
-    const update = {title: title, description: description, group: group, owner: owner};
-    const document = await roomModel.findByIdAndUpdate({_id: id}, update, (err,doc) => {
-      if(err) console.error(err);
-      console.log(doc);
-      return doc;
-    })
-    console.log(document);
-    
-    return document;
-},
+	updateRoom: async ({ id, title, description, group, owner }) => {
+		const update = {
+			title: title,
+			description: description,
+			group: group,
+			owner: owner,
+		};
+		const document = await roomModel.findByIdAndUpdate(
+			{ _id: id },
+			update,
+			(err, doc) => {
+				if (err) console.error(err);
+				console.log(doc);
+				return doc;
+			}
+		);
+		console.log(document);
 
+		return document;
+	},
+};
 
-}
-
-app.use('/graphql', graphqlHTTP({
-    schema: typeDefs,
-    rootValue: root,
-    graphiql: true,
-  }));
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema: typeDefs,
+		rootValue: root,
+		graphiql: true,
+	})
+);
 
 app.listen(PORT, () => {
-    console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`);
-})
+	console.log(
+		`Running a GraphQL API server at http://localhost:${PORT}/graphql`
+	);
+});
--- frontend/src/queries/AddRoom.js
+++ frontend/src/queries/AddRoom.js
@@ -1,129 +1,139 @@
-import React, { useState } from 'react';
-import gql from 'graphql-tag';
-import { useMutation } from '@apollo/react-hooks';
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
 
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import img1 from "../images/pic01.jpg";
+import img2 from "../images/pic02.jpg";
+import img3 from "../images/pic03.jpg";
+import img4 from "../images/pic04.jpg";
 
-import img1 from '../images/pic01.jpg';
-import img2 from '../images/pic02.jpg';
-import img3 from '../images/pic03.jpg';
-import img4 from '../images/pic04.jpg';
-
-const images = [img1, img2, img3, img4]
-
+const images = [img1, img2, img3, img4];
 
 const ADD_ROOM = gql`
-  mutation($title: String!, $description: String!, $group: String!, $owner: String!) {
-    addRoom(title: $title description: $description group: $group owner: $owner) {
-      title
-      description
-      group
-      owner
-    }
-  }
+	mutation(
+		$title: String!
+		$description: String!
+		$group: String!
+		$owner: String!
+	) {
+		addRoom(
+			title: $title
+			description: $description
+			group: $group
+			owner: $owner
+		) {
+			title
+			description
+			group
+			owner
+		}
+	}
 `;
 
 export default function AddRoom() {
+	const [addRoom, { data }] = useMutation(ADD_ROOM);
+	const [state, setState] = useState({
+		title: "",
+		description: "",
+		group: "",
+		owner: "",
+	});
 
-    const [addRoom, { data }] = useMutation(ADD_ROOM);
-    const [state, setState] = useState({
-        title: '',
-        description: '',
-        group: '',
-        owner: ''
-      })
+	const refreshPage = () => {
+		window.location.reload(false);
+	};
 
-    const refreshPage = () => {
-        window.location.reload(false)
-    }
+	function handleChange(e) {
+		const value = e.target.value;
+		setState({
+			...state,
+			[e.target.name]: value,
+		});
+	}
 
+	function handleSubmit(e) {
+		e.preventDefault();
+		addRoom({
+			variables: {
+				title: state.title,
+				description: state.description,
+				group: state.group,
+				owner: state.owner,
+			},
+		});
+		refreshPage();
+	}
 
-    function handleChange(e) {
-        const value = e.target.value
-        setState({
-            ...state,
-            [e.target.name]: value
-        });
-    }
+	function displayImages() {
+		let i = 0;
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        addRoom({ variables: {
-            title: state.title,
-            description: state.description,
-            group: state.group,
-            owner: state.owner}});
-            refreshPage();
-    }
+		return images.map((image) => {
+			return (
+				<option key={image}>{i++ < images.length && `pic0${i}`}</option>
+			);
+		});
+	}
 
-    function displayImages() {
-        let i = 0;
-        
-            return images.map(image => {
-                return (
-                <option
-                    key={image}
-                >
-                {i++ < images.length && `pic0${i}`}
-                </option>
-        )})
-    }
-
-    return (
-        <form onSubmit={handleSubmit} className="room-section" style={{backgroundColor: 'rgb(245, 245, 245)', width: '50%'}} >
-            <div className="room-content room-image">
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className="room-section"
+			style={{ backgroundColor: "rgb(245, 245, 245)", width: "50%" }}
+		>
+			{/* <div className="room-content room-image">
                 <label style={{marginRight: '10px'}}>Image:</label>
                 <select name="image">
                     {
                         displayImages()
                     }
                 </select>
-            </div>
-            <div className="room-content room-title">
-                <label style={{marginRight: '10px'}}>Title:</label>
-                <input
-                    style={{padding: '3px 5px'}}
-                    type="text"
-                    name="title"
-                    value={state.title}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="room-content room-description">
-                <label style={{marginRight: '10px'}}>Description:</label>
-                <input
-                    style={{padding: '3px 5px'}}
-                    type="text"
-                    name="description"
-                    value={state.description}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="room-content room-group">
-                <label style={{marginRight: '10px'}}>Group:</label>
-                <input
-                    style={{padding: '3px 5px'}}
-                    type="text"
-                    name="group"
-                    value={state.group}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="room-content room-owner">
-                <label style={{marginRight: '10px'}}>Owner:</label>
-                <input
-                    style={{padding: '3px 5px'}}
-                    type="text"
-                    name="owner"
-                    value={state.owner}
-                    onChange={handleChange}                />
-            </div>
-            <div className="room-content room-connect">
-                <input
-                    type="submit" value="Create Room"/>
-            </div>
-        </form>
-    )
+            </div> */}
+			<div className="room-content room-title">
+				<label style={{ marginRight: "10px" }}>Title:</label>
+				<input
+					style={{ padding: "3px 5px" }}
+					type="text"
+					name="title"
+					value={state.title}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="room-content room-description">
+				<label style={{ marginRight: "10px" }}>Description:</label>
+				<input
+					style={{ padding: "3px 5px" }}
+					type="text"
+					name="description"
+					value={state.description}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="room-content room-group">
+				<label style={{ marginRight: "10px" }}>Group:</label>
+				<input
+					style={{ padding: "3px 5px" }}
+					type="text"
+					name="group"
+					value={state.group}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="room-content room-owner">
+				<label style={{ marginRight: "10px" }}>Owner:</label>
+				<input
+					style={{ padding: "3px 5px" }}
+					type="text"
+					name="owner"
+					value={state.owner}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="room-content room-connect">
+				<input type="submit" value="Create Room" />
+			</div>
+		</form>
+	);
 }
